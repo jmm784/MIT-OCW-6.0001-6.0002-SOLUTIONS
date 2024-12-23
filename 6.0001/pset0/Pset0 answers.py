b7fe0d906508d11b2bd8b07ef50013f8889df29d
jmm784 <josephmason784@gmail.com>
2024-12-23 09:25:38 +0000

    Add files via upload

---
+++ 6.0001/pset0/Pset0 answers.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 13 19:57:30 2022
+
+@author: Joseph Mason
+"""
+
+#pset0 assignment answer
+import math
+x = float(input("Enter number x here:"))
+y = float(input("Enter number y here:"))
+print("x**y:", x**y)
+print("log(x, 2) =", math.log(x, 2))
